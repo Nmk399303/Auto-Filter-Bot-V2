36fa85689c46f8f6ac06aedc3ec0368bfe1cf5f7
Nmk399303 <78946350+Nmk399303@users.noreply.github.com>
2021-05-13 10:24:53 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -62,7 +62,8 @@ async def filter(client: Bot, message: Message):
                 [InlineKeyboardButton(text="📃 Pages 1/1",callback_data="pages")]
             )
             await message.reply_text(
-                f"<b> Here is the result for {message.text}</b>",
+                f"<b> ➠Cʜᴀɴɴᴇʟ : @cinema_festivals
+ Here is the result for {message.text}</b>",
                 reply_markup=InlineKeyboardMarkup(buttons)
             )
             return
@@ -78,7 +79,7 @@ async def filter(client: Bot, message: Message):
         )
 
         await message.reply_text(
-                f"<b> Here is the result for {message.text} ➠Cʜᴀɴɴᴇʟ : @cinema_festivals </b>",
+                f"<b> Here is the result for {message.text}  </b>",
                 reply_markup=InlineKeyboardMarkup(buttons)
             )    
 
