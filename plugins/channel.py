2137c8929ad69923cbcbe09d1c4647023a79a59b
Nmk399303 <78946350+Nmk399303@users.noreply.github.com>
2021-05-15 10:39:39 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -115,14 +115,14 @@ async def addchannel(client: Bot, message: Message):
 
     # from B to MiB
             
-            if file_size < 1024:
+            "if file_size < 1024:
                 file_size = f"[{file_size} B]"
             elif file_size < (1024**2):
                 file_size = f"[{str(round(file_size/1024, 2))} KiB] "
             elif file_size < (1024**3):
                 file_size = f"[{str(round(file_size/(1024**2), 2))} MiB] "
             elif file_size < (1024**4):
-                file_size = f"[{str(round(file_size/(1024**3), 2))} GiB] "
+                file_size = f"[{str(round(file_size/(1024**3), 2))} GiB] "" 
 
     docs = []
 
