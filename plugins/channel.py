852a6c1fbfdd5f4c7321179fed617d8159bc7cfa
Nmk399303 <78946350+Nmk399303@users.noreply.github.com>
2021-05-15 10:37:22 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -113,6 +113,17 @@ async def addchannel(client: Bot, message: Message):
         await intmsg.edit_text("Channel already added to db!")
         return
 
+    # from B to MiB
+            
+            if file_size < 1024:
+                file_size = f"[{file_size} B]"
+            elif file_size < (1024**2):
+                file_size = f"[{str(round(file_size/1024, 2))} KiB] "
+            elif file_size < (1024**3):
+                file_size = f"[{str(round(file_size/(1024**2), 2))} MiB] "
+            elif file_size < (1024**4):
+                file_size = f"[{str(round(file_size/(1024**3), 2))} GiB] "
+
     docs = []
 
     if DOC_SEARCH == "yes":
